933c906c854625feafaf2552653098addbaa769e
chasedurrett <chase.durrett80@gmail.com>
2020-05-28 16:14:41 -0500

    init commit

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+    this.state = {
+      students: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then((res) => {
+        this.setState({
+          students: res.data,
+        });
+      });
   }
 
   render() {
+    const students = this.state.students.map((e, i) => {
+      return (
+        <Link to={`/student/${e.id}`} key={i}>
+          <h3>
+             {e.first_name} {e.last_name}
+          </h3>
+        </Link>
+      );
+    });
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
-    )
+    );
   }
 }
